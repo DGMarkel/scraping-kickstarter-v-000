fd5e4f2981f4aeb47e3e55d308edb67b9e303307
David Markel <davidgmarkel@gmail.com>
2018-04-02 17:04:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 end
-binding.pry
-create_project_hash
 
+create_project_hash
+binding.pry
